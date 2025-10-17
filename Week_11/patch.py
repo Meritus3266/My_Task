175854d4f270c1a4572cef46ee8a6a14f783eeec
John Doe <solomon@example.com>
2025-10-17 15:18:03 +0100

    adding data

--- Week_11/patch.py
+++ Week_11/patch.py
@@ -1,74 +1,3 @@
-<<<<<<< HEAD
-from http.server import baseHTTPRequestHandler, HTTPServer
-import json
-
-data = [
-    {
-        "name": "sam lary",
-        "track": "AI Developer"
-    }
-]
-
-
-class BasicAPI(BaseHTTPRequestHandler):
-    def send_data(self, data, status - 201):
-        self.send_header("content Type", "application/json")
-        self.end_headers()
-        self.wfile.write(json.dumps(data).encode())
-        def.do_PATCH(self):
-            content_size = int(self.headers.get("content-length", 0))
-            parsed_data = self.rfile.read(content_size)
-
-            patched_data = json.loads(parsed_data)
-            if data:
-                data[0].update(patched_data)
-                self.send_data({
-                    "Message": "Data Edited"
-                    "data": data[0]
-                }, status=201)
-            else: 
-                self.send_data({
-                    "Message": "No data to patch"
-                }, status=400)
-def run():
-        HTTPSserver(('0.0.0.0', 8000), BasicAPI).server_forever()
-print("Application is running")
-run()        
-                
-=======
-# from http.server import BaseHTTPRequestHandler, HTTPServer
-# import json
-# data = [
-#     {
-#         "name": "sam lary",
-#         "track": "AI Developer"
-#     }
-# ]
-# class BasicAPI(BaseHTTPRequestHandler):
-#     def send_data(self, data, status - 201):
-#         self.send_header("content Type", "application/json")
-#         self.end_headers()
-#         self.wfile.write(json.dumps(data).encode())
-    
-#     def do_PATCH(self):
-#             content_size = int(self.headers.get("content-length", 0))
-#             parsed_data = self.rfile.read(content_size)
-#             patched_data = json.loads(parsed_data)
-#             if data:
-#                 data[0].update(patched_data)
-#                 self.send_data({
-#                     "Message": "Data Edited"
-#                     "data": data[0]
-#                 }, status=201)
-#             else:
-#                 self.send_data({
-#                     "Message": "No data to patch"
-#                 }, status=400)
-# def run():
-#         HTTPSserver(('0.0.0.0', 8000), BasicAPI).server_forever()
-# print("Application is running")
-# run()
-
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
 data = [
@@ -101,4 +30,4 @@ def run():
         HTTPServer(('0.0.0.0', 5000), BasicAPI).serve_forever()
 print("Application is running!")
 run()
->>>>>>> eb64b1b90ceaed9324c6aedb9df5f5d2e8e74b24
+
