a6058076b8b975b35e240a9f24e767c96607e7df
John Doe <solomon@example.com>
2025-10-21 17:22:59 +0100

    adding data

---
+++ Week_11/user.py
@@ -0,0 +1,55 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel, Field
+from sqlalchemy import text
+from Database import db
+import os
+from dotenv import load_dotenv
+import uvicorn
+import bcrypt
+#  Load environment variables
+load_dotenv()
+app = FastAPI(title="Simple App", version="1.0.0")
+#  Pydantic model
+class Simple(BaseModel):
+    name: str = Field(..., example="Olaiya Solomon")
+    email: str = Field(..., example="olaiyasolomon@email.com")
+    password: str = Field(..., example="olai234")
+
+@app.get("/", description="This endpoint just return a welcome message")
+def root():
+ return {"Message": "Welcome to my FastAPI App"}
+
+# Signup endpoint
+@app.post("/signup")
+def signUp(input: Simple):
+   
+    try:
+        # Check if email exists
+        duplicate_query = text("SELECT * FROM user WHERE email = :email")
+        existing = db.execute(duplicate_query, {"email": input.email}).fetchone()
+       
+        if existing:
+            raise HTTPException(status_code=400, detail="Email already exists")
+        # Hash password
+        salt = bcrypt.gensalt()
+        hashed_password = bcrypt.hashpw(input.password.encode('utf-8'), salt)
+        # Insert new user
+        query = text("""
+            INSERT INTO user (name, email, password)
+            VALUES (:name, :email, :password)
+        """)
+        db.execute(query, {"name": input.name, "email": input.email, "password": hashed_password})
+        db.commit()
+      
+        return {
+            "message": "User created successfully",
+            "data": {"name": input.name, "email": input.email}
+        }
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+#  Run app
+if __name__ == "__main__":
+    uvicorn.run(app, host=os.getenv("host"), port=int(os.getenv("port")))
+     
+       
+  
